8641a1f5029da4dc9a57a25b81495956c3ca80a7
ebal <gitlab.com@disposable.space>
2019-10-26 09:05:29 +0000

    Enable   https://doh.libredns.gr/ads

---
+++ roles/ads/files/update.stevenBlack.hosts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get StevenBlack hosts
+curl -sLo /tmp/hosts.txt https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+chmod 0644 /tmp/hosts.txt
+chown pdns:pdns /tmp/hosts.txt
+
+# Get diff
+diff -q <(sort -V /etc/powerdns/hosts.txt | column -t) <(sort -V /tmp/hosts.txt | column -t)
+DIFF_STATUS=$?
+
+# Get Lines
+LINES=`grep -c ^ /tmp/hosts.txt`
+
+# Check & restart if needed
+if [ "${LINES}" -gt "200" -a "${DIFF_STATUS}" != "0" ]; then
+    mv -f /tmp/hosts.txt /etc/powerdns/hosts.txt && systemctl restart pdns-recursor-ads
+fi
