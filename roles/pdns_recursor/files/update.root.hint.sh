c340e1edc65f49452ca06268e899fe23d76d11b1
Evaggelos Balaskas <3bal_gpg@disposable.space>
2020-12-24 18:58:21 +0200

    Update root hint script to get tier-1 fron ns0

--- roles/pdns_recursor/files/update.root.hint.sh
+++ roles/pdns_recursor/files/update.root.hint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get OpenNic Tier1 NS
-dig . NS @{{ OPENIC_ROOTNS }} | grep -Ev '^;|^$' | sort -u -V | column -t > /tmp/root.hint
+dig . NS @ns0.opennic.glue | grep -Ev '^;|^$' | sort -u -V | column -t > /tmp/root.hint
 
 # Get diff
 diff -q <(sort -V /etc/powerdns/root.hints | column -t) /tmp/root.hint
@@ -10,7 +10,8 @@ DIFF_STATUS=$?
 # Get Lines
 LINES=`grep -c ^ /tmp/root.hint`
 
-# Check & restart if needed
-if [ "${LINES}" -gt "20" -a "${DIFF_STATUS}" != "0" ]; then
+# Check & restart if needed, at least 3 NS must exist
+if [ "${LINES}" -gt "8" -a "${DIFF_STATUS}" != "0" ]; then
     mv -f /tmp/root.hint /etc/powerdns/root.hints && systemctl restart pdns-recursor
 fi
+
