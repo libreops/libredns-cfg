a30733ec6047c9597fd22649f4d827a15773a35b
Evaggelos Balaskas <3bal_gpg@disposable.space>
2022-11-28 15:04:06 +0200

    Adding malicious list to NoAds

--- roles/pdns_recursor/files/update.stevenBlack.hosts.sh
+++ roles/pdns_recursor/files/update.stevenBlack.hosts.sh
@@ -2,8 +2,6 @@
 
 # Get StevenBlack hosts
 curl -sLo /tmp/hosts.txt https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-chmod 0644 /tmp/hosts.txt
-chown pdns:pdns /tmp/hosts.txt
 
 # Get diff
 diff -q <(sort -V /etc/powerdns/hosts.txt | column -t) <(sort -V /tmp/hosts.txt | column -t)
@@ -14,5 +12,8 @@ LINES=`grep -c ^ /tmp/hosts.txt`
 
 # Check & restart if needed
 if [ "${LINES}" -gt "200" -a "${DIFF_STATUS}" != "0" ]; then
+    cat /etc/powerdns/predator.txt >> /tmp/hosts.txt
+    chmod 0644 /tmp/hosts.txt
+    chown pdns:pdns /tmp/hosts.txt
     mv -f /tmp/hosts.txt /etc/powerdns/hosts.txt && systemctl restart pdns-recursor-ads
 fi
