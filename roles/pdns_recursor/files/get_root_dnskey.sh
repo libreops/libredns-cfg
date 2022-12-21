bc16d9e5564527c6205d269a6f3d1ff595a53af2
Evaggelos Balaskas <3bal_gpg@disposable.space>
2022-12-21 14:30:44 +0200

    Update curl to retry when querying Opennic root NS

--- roles/pdns_recursor/files/get_root_dnskey.sh
+++ roles/pdns_recursor/files/get_root_dnskey.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-i=0
 OPENNIC_ROOTNS=""
+OPENNIC_ROOTNS=$(curl -sL --retry 10 --retry-delay 5 'https://api.opennicproject.org/geoip/?bare&res=1')
 
-while [ -z "$OPENNIC_ROOTNS" ]
-do
-    OPENNIC_ROOTNS=$(curl -sL 'https://api.opennicproject.org/geoip/?bare&res=1')
+# Exit script if in the end OPENNIC_ROOTNS is empty
+if [ -z "$OPENNIC_ROOTNS" ]; then
+    echo " Failed to get OpenNic RootNS"
+    exit 1
+fi
 
-    ((i++))
-    if [[ "$i" == '10' ]]; then
-        echo " Failed to get OpenNic RootNS"
-        exit 1
-    fi
-done
-
+# Get Opennic DNSKEY
 dig . DNSKEY @"$OPENNIC_ROOTNS" +noall +answer +additional > /etc/powerdns/OPENNIC.key
+# Merge Both root NS and Opennic root NS
 cat /usr/share/dns/root.key /etc/powerdns/OPENNIC.key > /etc/powerdns/root.key
+# Fix file permissions
 chown pdns:pdns /etc/powerdns/root.key
 
--- roles/pdns_recursor/files/get_root_hints.sh
+++ roles/pdns_recursor/files/get_root_hints.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-i=0
 OPENNIC_ROOTNS=""
+OPENNIC_ROOTNS=$(curl -sL --retry 10 --retry-delay 5 'https://api.opennicproject.org/geoip/?bare&res=1')
 
-while [ -z "$OPENNIC_ROOTNS" ]
-do
-    OPENNIC_ROOTNS=$(curl -sL 'https://api.opennicproject.org/geoip/?bare&res=1')
+# Exit script if in the end OPENNIC_ROOTNS is empty
+if [ -z "$OPENNIC_ROOTNS" ]; then
+    echo " Failed to get OpenNic RootNS"
+    exit 1
+fi
 
-    ((i++))
-    if [[ "$i" == '10' ]]; then
-        echo " Failed to get OpenNic RootNS"
-        exit 1
-    fi
-done
-
+# Get Openic root NS
 dig . NS @"$OPENNIC_ROOTNS" +noall +answer +additional > /etc/powerdns/root.hints
 
