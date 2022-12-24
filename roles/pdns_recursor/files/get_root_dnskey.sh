eb062116a4b9b3f5a6f142423efd1efbbb829a04
Evaggelos Balaskas <3bal_gpg@disposable.space>
2022-12-24 17:53:30 +0200

    Adding nosplit to dig command to get DNSKEY
    In order to get the entire DNSKEY from the root NameServers we need to add
    nosplit option in dig command so it will not split it to sections.

--- roles/pdns_recursor/files/get_root_dnskey.sh
+++ roles/pdns_recursor/files/get_root_dnskey.sh
@@ -5,12 +5,12 @@ OPENNIC_ROOTNS=$(curl -sL --retry 10 --retry-delay 5 'https://api.opennicproject
 
 # Exit script if in the end OPENNIC_ROOTNS is empty
 if [ -z "$OPENNIC_ROOTNS" ]; then
-    echo " Failed to get OpenNic RootNS"
+    echo "Failed to get OpenNic RootNS"
     exit 1
 fi
 
 # Get Opennic DNSKEY
-dig . DNSKEY @"$OPENNIC_ROOTNS" +noall +answer +additional > /etc/powerdns/OPENNIC.key
+dig . DNSKEY @"$OPENNIC_ROOTNS" +nosplit +noall +answer +additional > /etc/powerdns/OPENNIC.key
 # Merge Both root NS and Opennic root NS
 cat /usr/share/dns/root.key /etc/powerdns/OPENNIC.key > /etc/powerdns/root.key
 # Fix file permissions
